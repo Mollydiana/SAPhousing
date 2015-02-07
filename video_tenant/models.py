8e96ae03d1081df57ec2215de02947f5f683204b
IsabelleRey <isabellrey@gmail.com>
2015-02-07 14:46:24 -0800

    models

--- video_tenant/models.py
+++ video_tenant/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from geoposition.fields import GeopositionField
 
 # Create your models here.
+
+import datetime
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+
+class People(AbstractUser):
+    LANDLORD = 0
+    TENANT = 1
+    TYPE = (
+        (LANDLORD, "landlord"),
+        (TENANT, "tenant")
+    )
+    user_type = models.PositiveSmallIntegerField(choices=TYPE, default=0)
+    name = models.CharField(max_length=100, blank=True, null=True)
+
+class Issue(models.Model):
+    date = models.DateField(default=datetime.date.today())
+    user = models.ForeignKey(People, related_name='issues')
+    type = models.TextField(max_length=50, blank=True, null=True)
+    status_issue = models.IntegerField(default=0)
+    position = GeopositionField()
