6b027723d39ed92c62e37dfd530b452d180c1ace
yenbryan <yenbryan@gmail.com>
2015-02-07 19:23:55 -0800

    fixed emailing stuff

--- video_tenant/views.py
+++ video_tenant/views.py
@@ -6,6 +6,8 @@ from video_tenant.forms import AccountForm, AccountCreationForm
 """
 Shows the account page
 """
+
+
 def account(request):
     account_form = AccountForm()
     return render(request,'registration/account.html', {
@@ -15,12 +17,14 @@ def account(request):
 """
 Shows the registration form page!
 """
+
+
 def register(request):
     if request.method == 'POST':
         form = AccountCreationForm(request.POST)
         if form.is_valid():
             user = form.save()
-            user.send_email("welcome {}".format(user.username), "Thank you for signing up")
+            # user.send_email("welcome {}".format(user.username), "Thank you for signing up")
             return redirect("login")
     else:
         form = AccountCreationForm()
